7d67de5caacd20783da878e951cee58feb2849e5
AmTripathiDev <abhitripathi9800q@gmail.com>
2024-04-30 17:23:14 +0530

    update code in backend

--- src/app/employee/resolvers.ts
+++ src/app/employee/resolvers.ts
@@ -3,7 +3,7 @@ import { prismaClient } from "../db";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-  region: 'ap-south-1',
+  region: '',
   credentials: {
     secretAccessKey: "",
     accessKeyId: "",
